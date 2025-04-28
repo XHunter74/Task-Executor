8638910d8e55fc5f0546550c823c16bc27ca298f
serhiy.krasovskyy <xhunter74@gmail.com>
2025-04-28 09:20:27 +0300

    Added in-code documentations.

--- TaskExecutor/TaskExecutor.cs
+++ TaskExecutor/TaskExecutor.cs
@@ -2,6 +2,9 @@
 
 namespace TaskExecutor;
 
+/// <summary>
+/// This class is responsible for executing tasks concurrently.
+/// </summary>
 public class TaskExecutor : IDisposable
 {
     private readonly ConcurrentQueue<TaskForExecute> _taskQueue = new();
@@ -12,8 +15,14 @@ public class TaskExecutor : IDisposable
     private int _maxConcurrency;
     private bool _disposed;
 
+    /// <summary>
+    /// Event that is triggered when a task fails.
+    /// </summary>
     public event Action<object, Exception>? OnTaskError;
 
+    /// <summary>
+    /// Gets a value indicating whether there are any tasks currently running.
+    /// </summary>
     public bool HasRunningTasks
     {
         get
@@ -25,6 +34,21 @@ public class TaskExecutor : IDisposable
         }
     }
 
+    /// <summary>
+    /// Constructor for the TaskExecutor class.
+    /// </summary>
+    /// <param name="initialConcurrency">
+    /// Specifies the initial level of concurrency for the TaskExecutor. 
+    /// This determines how many tasks can run concurrently. 
+    /// Must be greater than 0.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// An optional CancellationToken that can be used to signal cancellation 
+    /// of task processing from an external source.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    /// Thrown when the initialConcurrency is less than or equal to 0.
+    /// </exception>
     public TaskExecutor(int initialConcurrency, CancellationToken cancellationToken = default)
     {
         if (initialConcurrency <= 0)
@@ -37,11 +61,36 @@ public class TaskExecutor : IDisposable
         StartProcessing();
     }
 
+    /// <summary>
+    /// Enqueues a task for execution.
+    /// </summary>
+    /// <param name="taskId">
+    /// The unique identifier for the task. This can be any object that represents the task's identity.
+    /// </param>
+    /// <param name="taskFunc">
+    /// A delegate representing the asynchronous function to be executed. The function must return a Task.
+    /// </param>
     public void EnqueueTask(object taskId, Func<Task> taskFunc)
     {
         _taskQueue.Enqueue(new TaskForExecute(taskId, taskFunc));
     }
 
+
+    /// <summary>
+    /// Adjusts the concurrency level of the TaskExecutor.
+    /// </summary>
+    /// <param name="newConcurrency">
+    /// The new concurrency level to set. This determines how many tasks can run concurrently.
+    /// Must be greater than 0.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    /// Thrown when the newConcurrency is less than or equal to 0.
+    /// </exception>
+    /// <remarks>
+    /// If the new concurrency level is higher than the current level, additional permits are released
+    /// to the semaphore to allow more tasks to run concurrently. If the new concurrency level is lower,
+    /// permits are acquired to reduce the number of tasks that can run concurrently.
+    /// </remarks>
     public void ChangeConcurrency(int newConcurrency)
     {
         if (newConcurrency <= 0)
@@ -53,10 +102,12 @@ public class TaskExecutor : IDisposable
 
             if (diff > 0)
             {
+                // Increase concurrency by releasing additional permits
                 _semaphore.Release(diff);
             }
             else if (diff < 0)
             {
+                // Decrease concurrency by acquiring permits
                 for (int i = 0; i < -diff; i++)
                 {
                     _semaphore.Wait();
@@ -120,6 +171,10 @@ public class TaskExecutor : IDisposable
         }
     }
 
+    /// <summary>
+    /// Stops the TaskExecutor and waits for all running tasks to complete.
+    /// </summary>
+    /// <returns></returns>
     public async Task StopAsync()
     {
         _internalCts.Cancel();
--- TaskExecutor/TaskForExecute.cs
+++ TaskExecutor/TaskForExecute.cs
@@ -1,10 +1,25 @@
 ﻿namespace TaskExecutor;
 
+/// <summary>
+/// This class represents a task that is to be executed by the TaskExecutor.
+/// </summary>
 public class TaskForExecute
 {
+    /// <summary>
+    /// The id of the task. This is used to identify the task in the queue.
+    /// </summary>
     public object Id { get; set; }
+
+    /// <summary>
+    /// The function to execute. This function should return a Task.
+    /// </summary>
     public Func<Task> TaskFunc { get; set; }
 
+    /// <summary>
+    /// The constructor for the TaskForExecute class.
+    /// </summary>
+    /// <param name="id">This parameter represents the unique identifier for the task. It can be any type since it is declared as object. This allows flexibility in how tasks are identified (e.g., using a string, integer, GUID, etc.).</param>
+    /// <param name="taskFunc">This parameter is a delegate that represents the function to be executed. The function must return a Task, making it asynchronous.</param>
     public TaskForExecute(object id, Func<Task> taskFunc)
     {
         Id = id;
